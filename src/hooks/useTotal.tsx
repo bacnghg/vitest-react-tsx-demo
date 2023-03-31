03dd276cb4cc8bdd2f6f536f27695aedb36ca9c6
bacnh7427 <bacnh7427@co-well.com.vn>
2023-03-31 15:34:02 +0700

    add test for use total

--- src/hooks/useTotal.tsx
+++ src/hooks/useTotal.tsx
@@ -1,9 +1,25 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from "react";
 
 export const useTotal = () => {
-    const [numberOne, setNumberOne] = useState(0)
-    const [numberTwo, setNumberTwo] = useState(0)
-    const [result, setResult] = useState(numberOne + numberTwo)
+  const [numberOne, setNumberOne] = useState(1);
+  const [numberTwo, setNumberTwo] = useState(2);
+  const [result, setResult] = useState(0);
 
-    return {numberOne, setNumberOne, numberTwo, setNumberTwo, result, setResult}
-}
+  useEffect(() => {
+    if (numberOne != 1) {
+        console.log('1111', { numberOne, numberTwo });
+    }
+  }, [numberOne]);
+
+  useEffect(() => {
+    if (numberTwo != 2) {
+        console.log('2222',{ numberOne, numberTwo });
+    }
+  }, [numberTwo]);
+
+  useEffect(() => {
+    setResult(numberOne + numberTwo);
+  }, [numberOne, numberTwo]);
+
+  return { setNumberOne, setNumberTwo, result };
+};
